72508438a4167b0525be440ea7b15d02cf41cd64
Adon Augustin <adonaugustin@gmail.com>
2025-10-08 13:22:04 +0100

    feat: implement card scoring functionality with popup and score management

--- powerup.js
+++ powerup.js
@@ -1,5 +1,5 @@
-var WHITE_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-white.svg';
-var BLACK_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-black.svg';
+var WHITE_ICON = 'https://adonaugustin.github.io/trello-powerup-score/icon.png';
+var BLACK_ICON = 'https://adonaugustin.github.io/trello-powerup-score/icon.png';
 
 var onBtnClick = function (t, opts) {
   console.log('Someone clicked the button');
