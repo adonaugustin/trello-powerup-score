84505e025add41d54498d11460b7dc05dbbf3a9a
Adon Augustin <adonaugustin@gmail.com>
2025-10-08 12:46:43 +0100

    refactor: update button definitions and add callback functionality in power-up

--- powerup.js
+++ powerup.js
@@ -1,24 +1,32 @@
+var WHITE_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-white.svg';
+var BLACK_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-black.svg';
+
+var onBtnClick = function (t, opts) {
+  console.log('Someone clicked the button');
+};
 
 window.TrelloPowerUp.initialize({
-  'board-buttons': function (t, options) {
+  'board-buttons': function (t, opts) {
     return [{
-      text: '👋 Say Hello',
-      callback: function (t) {
-        return t.alert({
-          message: 'Hello from your Trello Power-Up!',
-          duration: 3
-        });
-      }
-    }];
-  },
-
-  'card-buttons': function (t, options) {
-    return [{
-      text: '📋 Card Info',
-      callback: async function (t) {
-        const card = await t.card('name', 'id');
-        t.alert({ message: `Card: ${card.name}`, duration: 4 });
-      }
+      // we can either provide a button that has a callback function
+      icon: {
+        dark: WHITE_ICON,
+        light: BLACK_ICON
+      },
+      text: 'Callback',
+      callback: onBtnClick,
+      condition: 'edit'
+    }, {
+      // or we can also have a button that is just a simple url
+      // clicking it will open a new tab at the provided url
+      icon: {
+        dark: WHITE_ICON,
+        light: BLACK_ICON
+      },
+      text: 'URL',
+      condition: 'always',
+      url: 'https://trello.com/inspiration',
+      target: 'Inspiring Boards' // optional target for above url
     }];
   }
 });
